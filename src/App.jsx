717ab1221e3350807f2cf8f5150b787b42d849b3
payal <payalkapase.wr@gmail.com>
2025-03-12 12:03:07 +0530

    add new file

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react"
+import Card from "./Card"
+
+export default function App() {
+  const [istrue, setistrue] = useState(true)
+
+  const set = () => {
+    setistrue(!istrue)
+  }
+  console.log(istrue)
+  return (
+        <div className="">
+            <div className=" text-center font-serif  my-3 p-4">
+                 <h2 className="  text-xl py-2  
+                 sm:text-xl
+                 md:text-2xl
+                 lg:text-[22px] 
+                 xl:text-[26px] ">
+                 Plans & Pricing</h2>
+
+                 <h1 className=" py-2 font-serif font-bold  text-[16px] 
+                 sm:text-xl 
+                 md:text-2xl 
+                 lg:text-[26px] 
+                 xl:text-3xl ">
+                   Choose the Right Plan for Your Success!</h1>
+
+        <h5 className=" hidden py-2 
+        sm:text-[10px] sm:block
+        md:text-[12px] md:block
+        lg:text-[14px] lg:block
+        xl:text-[16px] xl:block "> 
+          Unlock Your Dream Job with AI-Powered Preparation – Tailored for Every Aspirant. <br /> Explore Our Plans & Start Your Journey Today!"</h5>
+        <div  >
+
+          <span className="mt-6 font-serif mr-4 text-[14px] lg:text-[16px] xl:text-[18px]">₹ INR</span>
+
+          <label  htmlFor="check" className="bg-blue-600  cursor-pointer relative px-7 top-0 pr-5 py-1 w-26 lg:h-7 rounded-full mt-8">
+            <input onClick={set} type="checkbox" id="check" className="sr-only peer" />
+            <span className=" w-[40%] h-[66%] bg-white absolute rounded-full left-1 top-1 px-1 peer-checked:bg-white
+              peer-checked:left-6 " ></span>
+          </label>
+
+          <span className="mt-6 font-serif ml-4 text-[14px] lg:text-[16px] xl:text-[18px]">$ USD</span>
+        </div>
+      </div>
+
+      <Card val={istrue}/> 
+    </div>
+  )
+}
---
+++ src/Card.jsx
@@ -0,0 +1,59 @@
+import data from '../Info.json'
+
+function Card( {val}) {
+    console.log(val)
+    return (
+       
+         <div className='  grid sm:grid-cols-2 sm:mx-10   md:grid-cols-2 md:ml-32 md:mx-20  lg:grid-cols-3 xl:grid-cols-4 xl:mx-[5rem]    '>
+            
+            {
+                data.map((plans, index) => (
+                    <div  key={index} className=" w-64 ml-8 mb-2 p-3  border-2  rounded-lg   hover:border-blue-600 shadow-md transition-transform transform hover:scale-105
+                                         sm:p-4  sm:mb-4 md:mr-20 md:mb-5 xl:w-[18rem] lg:p-5 lg:mb-5">
+
+
+                        <div  className="text-[18px]  pb-2 font-serif text-2xl font-bold text-center 
+                        sm:text-[18px] sm:pb-3
+                        md:text-[20px] md:pb-4
+                        lg:text-[22px] lg:pb-4
+                        xl:text-[24px] xl:pb-5">
+
+                            <h1>{plans.name}</h1>
+                            <h1> <span className='font-sans'>{val==true ? plans.price1: plans.price2}</span> <span className="text-[13px] from-neutral-100 ">/year</span></h1>
+
+                        </div>
+
+                        <div className='text-center'>
+
+                            <ul className=" mt-2 ml-8 mb-8   font-serif text-[15px] h-32  text-left  
+                             sm:text-[14px] sm:h-40 sm:mb-2
+                             md:text-[16px] md:h-44 md:mb-4
+                             lg:text-[17px] lg:h-52 lg:mb-2
+                             xl:text-[18px] xl:h-64 xl:text-center">
+                                {
+                                    plans.features.map((feature, i) => (
+                                        <li key={i} className='flex '>
+                                             <span className="w-3 h-3 flex items-center justify-center bg-blue-500
+                                              text-white rounded-full text-[8px] font-bold mr-2 mt-2"> ✔ </span>
+                                             <span className='text-left'>{feature}</span>
+                                            </li>
+                                    ))
+                                }
+                            </ul>
+
+                            <button className="m-3 px-4 py-2 w-28 bg-blue-700 text-white rounded-lg hover:bg-black
+                                                sm:w-32 md:w-36 md:mx-4 lg:w-48 lx:pb-5">
+                                Explore
+                            </button>
+                        </div>
+                    </div>
+                ))
+            }
+        <div>
+  </div>
+</div>
+
+    )
+}
+
+export default Card;
